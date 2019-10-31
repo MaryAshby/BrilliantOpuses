995c1b4a36cda94cd570d94e236c9e0654945c80
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-31 02:46:14 -0400

    Update NYTBSL.js

--- NYTBSL.js
+++ NYTBSL.js
@@ -1,14 +1,21 @@
-
+var setgoodNews = function(message)
+{
+    d3.select(".goodNews").text(message);
+}
 
 var listPromise = 
     d3.json("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
          listPromise.then(function(listData)
 {
           console.log ("works1", listData);
+               setgoodNews("Data has successfully loaded");
+             
 },
           function(err)
 {
-          console.log("broke1",err)
+          console.log("broke1",err);
+             setgoodNews("Houston, we have a problem.");
+             
 })
                          
 var listPromise = 
@@ -19,7 +26,7 @@ var listPromise =
 },
           function(err)
 {
-          console.log("broke2",err)
+          console.log("broke2",err);
 })
 
 var listPromise = 
@@ -30,7 +37,7 @@ var listPromise =
 },
           function(err)
 {
-          console.log("broke3",err)
+          console.log("broke3",err);
 })
 
  var listPromise = 
@@ -41,7 +48,7 @@ var listPromise =
 },
           function(err)
 {
-          console.log("broke4",err)
+          console.log("broke4",err);
 })           
 
 
