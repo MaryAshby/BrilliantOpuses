610440862acf7a8b58307c8ea5ba5c1719a32f92
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-30 21:21:24 -0400

    dropdown

--- NYTBSL.js
+++ NYTBSL.js
@@ -5,7 +5,7 @@ var setBanner = function(message)
              d3.select("#banner").text(message);
             }
 
-var listPromise = d3.jason("https://api.nytimes.com/svc/books/v3/lists.json")
+var listPromise = d3.json("https://api.nytimes.com/svc/books/v3/lists.json")
          listPromise.then(function(NYTlist)
             {
             setBanner("Search the New York Times Best Seller Lists");
@@ -15,9 +15,15 @@ var listPromise = d3.jason("https://api.nytimes.com/svc/books/v3/lists.json")
             setBanner("NYT Best Seller Lists Currently Not Available");
              });
 
+            .show 
+            {
+             display:block;
+            }
 
-
-
+var drop = listFunction() 
+            {
+            document.getElementById("listDropDown").classList.toggle("show");
+            }
 
 
 
