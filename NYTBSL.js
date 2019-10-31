a08e432285f892a06999de94cb08996325323675
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-30 20:04:43 -0400

    Created Promise

--- NYTBSL.js
+++ NYTBSL.js
@@ -1,1 +1,24 @@
+//key 1koWnqTA52klgXEgxygUqAjVc372SrnL//
+
+var setBanner = function(message)
+            {
+             d3.select("#banner").text(message);
+            }
+
+var listPromise = d3.jason("https://api.nytimes.com/svc/books/v3/lists.json")
+         listPromise.then(
+         function(NYTlist)
+            {
+            setBanner("Search the New York Times Best Seller Lists");
+             },
+          function(err)
+            {
+            setBanner("NYT Best Seller Lists Currently Not Available");
+             });
+
+
+
+
+
+
 
