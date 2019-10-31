62ec0375e92683754dad30283ff19f81f2ceff09
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-31 02:37:16 -0400

    all links

--- NYTBSL.js
+++ NYTBSL.js
@@ -1,21 +1,48 @@
 
 
 var listPromise = 
-    d3.json("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
+    d3.json("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
          listPromise.then(function(listData)
 {
-          console.log ("works", listData);
+          console.log ("works1", listData);
 },
           function(err)
 {
-          console.log("broke",err)
+          console.log("broke1",err)
 })
                          
+var listPromise = 
+    d3.json("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
+         listPromise.then(function(listData)
+{
+          console.log ("works2", listData);
+},
+          function(err)
+{
+          console.log("broke2",err)
+})
 
+var listPromise = 
+    d3.json("https://api.nytimes.com/svc/books/v3/lists/current/young-adult-hardcover.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
+         listPromise.then(function(listData)
+{
+          console.log ("works3", listData);
+},
+          function(err)
+{
+          console.log("broke3",err)
+})
 
-
-
-            
+ var listPromise = 
+    d3.json("https://api.nytimes.com/svc/books/v3/lists/current/graphic-books-and-manga.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
+         listPromise.then(function(listData)
+{
+          console.log ("works4", listData);
+},
+          function(err)
+{
+          console.log("broke4",err)
+})           
 
 
 //calls//
