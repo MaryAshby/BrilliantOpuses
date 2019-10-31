420d654e8ec7bc185eb8ff16568491edd4090ce7
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-31 02:18:03 -0400

    Update NYTBSL.js

--- NYTBSL.js
+++ NYTBSL.js
@@ -4,20 +4,12 @@ var key = ".json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL";
 
 var baseURL =  "https://api.nytimes.com/svc/books/v3/lists/current/";
 
-var listPromise = d3.json("baseURL"+"name"+"key");
-          
-
-var listed = d3.select(".putHere")
-    .selectAll("p")
-    .data(killMeNow)
-    .enter()
-    .append("a")
-    .atttr("href", function(d)
-          {return d.listed})
-}
+fetch('https://api.nytimes.com/svc/books/v3/ists/current/hardcover-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL, {    method: 'get',  }) 
+          .then(response => { return response.json(); })  
+          .then(json => { console.log(json); });
 
 
-       
+
 
 
             
