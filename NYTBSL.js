ba72c97546c6fdf984e811c6f7d8ccc95d711623
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-31 02:13:14 -0400

    Update NYTBSL.js

--- NYTBSL.js
+++ NYTBSL.js
@@ -1,14 +1,22 @@
+var name = document.getElementById("BSform").value;
 
 var key = ".json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL";
 
 var baseURL =  "https://api.nytimes.com/svc/books/v3/lists/current/";
 
-var name = document.getElementById("BSform").value;
+var listPromise = d3.json("baseURL"+"name"+"key");
+          
 
-var win = function(thismaykillme)
-{ var request="baseURL" + "name" + "key";
-  return();
+var listed = d3.select(".putHere")
+    .selectAll("p")
+    .data(killMeNow)
+    .enter()
+    .append("a")
+    .atttr("href", function(d)
+          {return d.listed})
 }
+
+
        
 
 
