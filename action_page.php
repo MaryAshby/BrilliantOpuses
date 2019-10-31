93a56c84bca22fca711d9a7b324b5a3331a2d546
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-30 23:35:14 -0400

    initial

--- action_page.php
+++ action_page.php
@@ -1,1 +1,12 @@
-
+<html>
+  <body>
+    
+    <?php echo $_GET["List"]; ?><br>
+    <?php echo $_GET["bSellDate"]; ?><br>
+    <?php echo $_GET["pubDate"]; ?>
+    
+    </body>
+  <html>
+    
+  
+      
