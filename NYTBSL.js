6fadb212b47574a7e24081eb6c4df248f4906db6
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-11-03 06:12:08 -0500

    Update NYTBSL.js

--- NYTBSL.js
+++ NYTBSL.js
@@ -1,158 +1,58 @@
-//load all data and make sure it works//
+/*Check load of data. Use var to make easy retrival of 
+message depending on later promise and put it in p class goodNews*/
 
 var setgoodNews = function(message)
-{
-    d3.select(".goodNews").text(message);
-}
-
-//Table Creation//
-//makes header and allows sort by headers//
-
-var sortColumn = function(books, col, accessor)
-{
-    d3.select(col)
-      .on("click", function()
-          {
-          books.sort(function(a,b)
-                     {
-                     return(a-b);
-                     })
-        makeTable(books,"ALL");
-    })
-}
-
-var makeTableHeader = function(books)
-{
-    d3.select("#rank")
-      .on("click", function()
-       {
-        makeTable(books.sort(function(a, b)
-                             {
-                              return a,b
-                              }), "ALL")
-})
-}
-    
- 
- //add columns & rows to table//
-    
- var addCol = function(rows, fcn)
- {
-     rows.append("td").text(fcn);
- }
- 
- 
- var makeTable = function(books, mode)
- {
-     d3.selectAll("tbody*").remove();
-     
-     var rows = d3.select("tbody")
-                  .selectAll("tr")
-                  .data(filterbooks(books, mode))
-                  .enter()
-                  .append("tr");
-     addCol(rows, function(books)
-            {
-             return book.rank
-            })
-            .attr("alt", function(books)
                   {
-                   return results.rank;
-                   })
-     addCol(rows, function(books){return results.book_details.title})
-     addCol(rows, function(books){return results.book_details.author})
-     addCol(rows, function(books){return results.book_details.description})
-     
- }
-     
+                   d3.select(".goodNews").text(message);
+                   }
 
-     
+//Make table header sort - used switching as described by 3wSchool//
 
-//onclick call//
-
-var getData = firstFunction() 
-{   
-   
-if (whatItBe = "combined-print-and-e-book-fiction")
-{     
-         d3.json("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
-         listPromise.then(function(listData));
-                          {
-          console.log ("works", listData);
-               setgoodNews("Data has successfully loaded");
-             
-},
-          function(err)
-{
-          console.log("broke",err);
-             setgoodNews("Houston, we have a problem.");
-             
-})
-    
-else if (whatItBe = "combined-print-and-e-book-nonfiction")
-    {
-         d3.json("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
-         listPromise.then(function(listData);
-                          {
-          console.log ("works", listData);
-               setgoodNews("Data has successfully loaded");
-             
-},
-          function(err)
-{
-          console.log("broke",err);
-             setgoodNews("Houston, we have a problem.");
-             
-})
-  
-    
-else if (whatItBe="young-adult-hardcover")
-{
-         d3.json("https://api.nytimes.com/svc/books/v3/lists/current/young-adult-hardcover.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
-         listPromise.then(function(listData)
-          {
-          console.log ("works", listData);
-               setgoodNews("Data has successfully loaded");
-             
-},
-          function(err)
-{
-          console.log("broke",err);
-             setgoodNews("Houston, we have a problem.");
-             
-})
-
-    
-       
-else if (whatItBe="graphic-books-and-manga")
-{
-     d3.json("https://api.nytimes.com/svc/books/v3/lists/current/graphic-books-and-manga.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL");
-         listPromise.then(function(listData)
-                          {
-          console.log ("works", listData);
-               setgoodNews("Data has successfully loaded");
-             
-},
-          function(err)
-{
-          console.log("broke",err);
-             setgoodNews("Houston, we have a problem.");
-             
-})
-
-       return makeTable
+var sorter = function sortTable(n) 
+              {
+               var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+               table = document.getElementById("putHere");
+               switching = true;
+               dir = "asc";
+               while (switching) 
+                   {
+                    switching = false;
+                    rows = table.rows;
+                    for (i = 1; i < (rows.length - 1); i++) 
+                       {
+                        shouldSwitch = false;
+                        x = rows[i].getElementsByTagName("rank")[n];
+                        y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
-
-
-  
- 
-                             
-
-
-
-//calls//
-//https://api.nytimes.com/svc/books/v3//
-
-//for current lists//
-//https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=1koWnqTA52klgXEgxygUqAjVc372SrnL//
-
